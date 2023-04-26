f7c389b797fa40877265deb0174b6a5767d3ca3a
Durjoy001 <durjoydey001@gmail.com>
2023-04-26 18:24:37 +0600

    refactor code

--- src/components/editBlog.tsx
+++ src/components/editBlog.tsx
@@ -79,58 +79,53 @@ export const EditBlog : FC<Props> = () => {
   if(isLoading){
       return <h1>loading...</h1>
   }
-  else if (!data) {
+  if (!data) {
     return <h1>Blog not found</h1>;
+  }  
+  if(!loggedIn || username !== data.creator){
+    return (<h1>don't have permission</h1>)
   }
-  else {
-      if(!loggedIn || username !== data.creator){
-          //return <Redirect to={'/blogs/view/' + id }/>  
-          return (<h1>don't have permission</h1>)
-      }
-      else{
-          return (
-              <form onSubmit={updateblog}>  
-                  <Stack
-                      spacing={4}
-                      p="5rem"
-                      backgroundColor="whiteAlpha.900"  
-                      boxShadow="md"    
-                  > 
-                      <Textarea 
-                          rows={3}
-                          variant="outline"
-                          name="name"
-                          value={name}
-                          onChange={(e) => setName(e.target.value)}
-                          autoFocus
-                          size="lg"
-                      />
-                      <Textarea
-                          rows={10}
-                          value = {description}  
-                          variant="outline"
-                          name= "description"
-                          onChange={(e) => setDescription(e.target.value)}
-                          size="lg"
-                      />
-                      {
-                          requestState === "error" && (
-                          <Text display="block" fontSize="sm" color="red">
-                          {error}
-                          </Text>
-                      )}
-                      <Button
-                          borderRadius={0}
-                          type="submit"
-                          variant="solid"
-                          colorScheme="teal"
-                          width="full"
-                      >
-                        UPDATE
-                      </Button>
-                  </Stack>
-              </form>
-          )
-      } 
-  }   
-}
+  return (
+      <form onSubmit={updateblog}>  
+          <Stack
+              spacing={4}
+              p="5rem"
+              backgroundColor="whiteAlpha.900"  
+              boxShadow="md"    
+          > 
+              <Textarea 
+                  rows={3}
+                  variant="outline"
+                  name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  autoFocus
+                  size="lg"
+              />
+              <Textarea
+                  rows={10}
+                  value = {description}  
+                  variant="outline"
+                  name= "description"
+                  onChange={(e) => setDescription(e.target.value)}
+                  size="lg"
+              />
+              {
+                  requestState === "error" && (
+                  <Text display="block" fontSize="sm" color="red">
+                  {error}
+                  </Text>
+              )}
+              <Button
+                  borderRadius={0}
+                  type="submit"
+                  variant="solid"
+                  colorScheme="teal"
+                  width="full"
+              >
+                UPDATE
+              </Button>
+          </Stack>
+      </form>
+  )
+} 
